1afca2fdfc50e88db5b3c1fa6691bf17e9135836
Nick Goncharenko <goncharenkonikolay@gmail.com>
2024-07-28 22:39:58 -0700

    feat: slices

--- src/03-data-types/20-slice-length-cap/main.go
+++ src/03-data-types/20-slice-length-cap/main.go
@@ -4,33 +4,35 @@ import "fmt"
 
 func main() {
 	s := make([]int, 3, 6)
-	print(s)
+	print("s", s)
 
 	s[1] = 1
-	print(s)
+	print("s", s)
 
 	s = append(s, 2)
-	print(s)
+	print("s", s)
 
 	s = append(s, 3, 4, 5)
-	print(s)
+	print("s", s)
 
 	s1 := make([]int, 3, 6)
 	s2 := s1[1:3]
 	s1[1] = 1
-	print(s2)
+	print("s1", s1)
+	print("s2", s2)
 
 	s2 = append(s2, 2)
-	print(s1)
-	print(s2)
+	print("s1", s1)
+	print("s2", s2)
 
 	s2 = append(s2, 3)
 	s2 = append(s2, 4)
 	s2 = append(s2, 5)
-	print(s1)
-	print(s2)
+	print("s1", s1)
+	print("s2", s2)
 }
 
-func print(s []int) {
-	fmt.Printf("len=%d, cap=%d: %v\n", len(s), cap(s), s)
+func print(name string, s []int) {
+	fmt.Printf("slice: %s, slice start addr: %p, underlying array addr: %p, len=%d, cap=%d: %v\n",
+		name, s, &s[0], len(s), cap(s), s)
 }
